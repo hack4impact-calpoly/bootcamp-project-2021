5f4bc8f664d4a332ac452d146b5e63e4bff56cbe
Noah Shah <noah.shah3@gmail.com>
2021-10-24 23:42:24 -0700

    created function for converting recipe to json

---
+++ recipes/recipes.js
@@ -0,0 +1,22 @@
+function recipeJSON() {
+  const recipe = {};
+  recipe["recipeName"] = document.getElementById("recipeName").innerHTML;
+  // recipe["recipeDescription"] = document.getElementById("recipeDescription").innerHTML;
+  recipe["recipeImage"] = document.getElementById("recipeImage").src;
+
+  const ingredientList = [];
+  const ingredients = document.getElementById("ingredientList").getElementsByTagName("li");
+  for (let i = 0; i < ingredients.length; i++) {
+    ingredientList.push(ingredients[i].innerHTML);
+  }
+  recipe["ingredientList"] = ingredientList;
+  document.getElementById("recipeJSONFormat").textContent = JSON.stringify(recipe, null, 4);
+
+  const directionsList = [];
+  const directions = document.getElementById("directionsList").getElementsByTagName("li");
+  for (let i = 0; i < directions.length; i++) {
+    directionsList.push(directions[i].innerHTML);
+  }
+  recipe["directionsList"] = directionsList;
+  document.getElementById("recipeJSONFormat").textContent = JSON.stringify(recipe, null, 4);
+}
