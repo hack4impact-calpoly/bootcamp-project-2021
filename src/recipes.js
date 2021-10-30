ff9b1747f5f913f11775038cf302f30b600a0cc6
Noah Yuen <noahyuen19@gmail.com>
2021-10-30 15:46:14 +0200

    finished instruction input

--- src/recipes.js
+++ src/recipes.js
@@ -10,3 +10,11 @@ let addIngredient = () => {
     // add new ingredient to list
     ul.appendChild(li);
 }
+
+let addInstruction = () => {
+    let ol = document.getElementById("instruction-list");
+    let li = document.createElement("li");
+    let newInstruction = document.getElementById("new-instruction").value;
+    li.appendChild(document.createTextNode(newInstruction));
+    ol.appendChild(li);
+}
