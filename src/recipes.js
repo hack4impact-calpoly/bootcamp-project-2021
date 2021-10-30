06e9adb11e99061e6179b2cda7b039f4dc36e25e
Noah Yuen <noahyuen19@gmail.com>
2021-10-30 21:06:08 +0200

    finishing up json conversion

--- src/recipes.js
+++ src/recipes.js
@@ -18,3 +18,37 @@ let addInstruction = () => {
     li.appendChild(document.createTextNode(newInstruction));
     ol.appendChild(li);
 }
+
+let convertToJSON = () => {
+    let conversion = {};
+    conversion.recipeName = document.getElementById("recipe-name").innerHTML;
+    conversion.recipeDescription = document.getElementById("recipe-description").innerHTML;
+    conversion.recipeImage = document.getElementById("recipe-image").innerHTML;
+
+    // create ingredient list
+    let ingredientList = document.getElementById("ingredient-list").getElementsByTagName("li");
+    console.log(ingredientList);
+    let tempList = []
+    for (let i = 0; i < ingredientList.length; i++) {
+        tempList.push(ingredientList[i].innerHTML);
+    }
+    conversion.ingredientList = tempList;
+
+    // create instruction list
+    let instructionList = document.getElementById("instruction-list").getElementsByTagName("li");
+    console.log(instructionList);
+    let tempList2 = []
+    for (let j = 0; j < instructionList.length; j++) {
+        tempList2.push(instructionList[j].innerHTML);
+    }
+    conversion.instructionList = tempList2;
+
+    // display json format
+    displayJSON(conversion);
+}
+
+let displayJSON = (conversion) => {
+    document.getElementById("conversion").textContent = JSON.stringify(conversion, null, 2);
+    document.getElementById("pre-format").style.padding = "20px";
+    document.getElementById("conversion").style.maxBlockSize = "300px";
+}
