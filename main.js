399552d1493fe375d07741e336ac1dd567fcb3c9
Rachel Izenson <rjizenson@gmail.com>
2021-10-30 17:33:45 -0700

    Final Milestone #2

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 let addToList = () => {
     let ul = document.getElementById("list");
-    let random = document.createElement("li");
+    let li = document.createElement("li");
     let ingredient = document.getElementById("newIngredient").value;
-    random.appendChild(document.createTextNode(ingredient));
-    ul.appendChild(random);
+    li.appendChild(document.createTextNode(ingredient));
+    ul.appendChild(li);
 }
 
 let addToListInstruction = () => {
     let ul = document.getElementById("instructionList");
-    let random = document.createElement("li");
+    let li = document.createElement("li");
     let instruction = document.getElementById("newInstruction").value;
-    random.appendChild(document.createTextNode(instruction));
-    ul.appendChild(random);
+    li.appendChild(document.createTextNode(instruction));
+    ul.appendChild(li);
 }
 
-
 let recipeInfo = {}
-
 function recipeToJSON() {
   recipeInfo.recipeName = document.getElementById('recipeName').innerHTML;
   recipeInfo.recipeDescription = document.getElementById('description').innerHTML;
@@ -24,39 +22,19 @@ function recipeToJSON() {
   recipeInfo.personalImage = document.getElementById('personalImage').src;
   recipeInfo.recipeLink = document.getElementById('url').href;
 
-  // get each ingredient
   var ingredientList = []
   var liTags = document.getElementById('list').getElementsByTagName('li');
   for (var key of Object.keys(liTags)) {
     ingredientList.push(liTags[key].innerHTML);
   }
-  recipeInfo.ingredientList = ingredientList;
+  recipeInfo.ingredients = ingredientList;
 
-
-  //get each instruction
   var instructionList = []
   var liTags = document.getElementById('instructionList').getElementsByTagName('li');
   for (var key of Object.keys(liTags)) {
     instructionList.push(liTags[key].innerHTML);
   }
-  recipeInfo.steps = instructionList;
+  recipeInfo.instructions = instructionList;
 
   document.getElementById("json").textContent = JSON.stringify(recipeInfo, undefined, 2);
-}
-
-
-function addIngredient() {
-  var ul = document.getElementById("ingredientList");
-  var li = document.createElement("li");
-  var ingredient = document.getElementById("newIngredient").value;
-  li.appendChild(document.createTextNode(ingredient));
-  ul.appendChild(li);
-}
-
-function addInstruction() {
-  var ol = document.getElementById("instructionList");
-  var li = document.createElement("li");
-  var ingredient = document.getElementById("newInstruction").value;
-  li.appendChild(document.createTextNode(ingredient));
-  ol.appendChild(li);
 }
