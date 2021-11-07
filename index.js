d64ae3fc492ab3f661909d4c9295ee2f0c252fb0
Carter Chan-Nui <carterchannui@gmail.com>
2021-11-07 14:59:27 -0800

    Added JSON conversion

--- index.js
+++ index.js
@@ -15,3 +15,35 @@ let addInstruction = () => {
     li.appendChild(document.createTextNode(ingredient));
     ul.appendChild(li);
 }
+
+let convertJSON = () => {
+    let recipeInfo = {}
+
+    recipeInfo.recipeName = document.getElementById("Recipe_Name").innerHTML;
+    console.log("Recipe name retrieved");
+    recipeInfo.recipeDescription = document.getElementById("Recipe_Description").textContent;
+    console.log("Recipe description retrieved");
+    recipeInfo.recipeImage = document.getElementById("Recipe_Image").src;
+    console.log("Recipe image retrieved.");
+    var recipeIngredientList = []
+    var liTags = document.getElementById("ingredients").getElementsByTagName("li");
+    for (let key of Object.keys(liTags)) {
+        recipeIngredientList.push(liTags[key].innerHTML);
+    }
+
+    var recipeInstructionList = []
+    var liTags = document.getElementById("instructions").getElementsByTagName("li");
+    for (let key of Object.keys(liTags)) {
+        recipeInstructionList.push(liTags[key].innerHTML);
+    }
+
+    recipeInfo.recipeIngredients = recipeIngredientList;
+    recipeInfo.recipeInstructions = recipeInstructionList;
+
+
+    document.getElementById("JSON_Display").textContent = JSON.stringify(recipeInfo, undefined, 2);
+}
+
+
+
+
