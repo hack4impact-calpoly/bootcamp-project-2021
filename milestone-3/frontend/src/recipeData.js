04581dde1acb495af569be5b111a1ee85366b907
Noah Yuen <noahyuen19@gmail.com>
2021-11-08 14:23:47 -0800

    adding ingredients and instructions

--- milestone-3/frontend/src/recipeData.js
+++ milestone-3/frontend/src/recipeData.js
@@ -3,25 +3,25 @@ const recipes = [
       "name": "Carbonara",
       "description": "Carbonara has by far been my favorite dish here in Rome. It actually originated in Rome so this is the best place to get it. The dish only requires a few simple ingredients, but is difficult to make well (trust me, I've tried). It's a simple pasta dish made from pasta, egg, cheese and pancetta.",
       "imageSrc": "https://www.negroni.com/sites/negroni.com/files/styles/scale__1440_x_1440_/public/carbonara_la_pasta_amata.jpg?itok=Iz2Me_rw",
-      "ingredients": ["bacon", "tomato"],
+      "ingredients": ["100g Pancetta", "50g Pecorino Cheese", "50g Parmesan", "3 Large Eggs", "350g Spaghetti", "2 Cloves of Peeled Garlic", "50g Unsalted Butter", "Sea Salt and Freshly Ground Black Pepper"],
       "servingSize": "2",
-      "instructions": ["buy the food", "eat the food"]
+      "instructions": ["Boil water in large saucepan", "Chop pancetta and grate cheese, mix together", "Beat eggs and set aside", "Add 1 tsp salt to water, add spaghetti and cook for 10 minutes", "Fry pancetta with garlic and butter for 5 minutes", "Mix eggs and cheese", "Pour eggs and cheese into pan with spaghetti and pancetta", "Cook until pasta is coated"]
     },
     {
       "name": "Pizza Margherita",
       "description": "The pizza here in Italy doesn't even compare to my usual Domino's order at home. It's much more thin than US pizza and it doesn't feel as heavy. My favorite pizza is made from the simple combination of flour, fresh tomato sauce, and mozarella cheese (optional).",
       "imageSrc": "https://blog.giallozafferano.it/francinut87/wp-content/uploads/2021/04/Pizza-margherita-fatta-in-casa-orizzontale.jpg",
-      "ingredients": [],
+      "ingredients": ["1 kg Italian '00' flour", "30g Fresh Yeast", "500 ml Water", "10g Salt", "80g Olive Oil", "4 Basil Leaves", "80g Mozzarella Cheese", "400g Canned Tomato", "Salt"],
       "servingSize": "2",
-      "instructions": []
+      "instructions": ["Combine the yeast, salt, warm water in a bowl", "Knead the Dough", "Let the dough rise for 3 hours", "Prepare tomato sauce with salt and olive oil", "Flatten dough into desired shape", "Spread sauce, mozarella and basil over the dough", "Bake at 480 degrees F for 5 or 6 minutes", "Remove the pizza from the oven and enjoy!"]
     },
     {
       "name": "Seafood Risotto",
       "description": "I recently had the BEST risotto of my life in a small town called Anzio. I certainly don't know how to make this dish, much less make it well. But I wanted to share this dish in case anyone out there hadn't heard of it before. It's a delicious combination of rice (risotto) and a variety of seafood.",
       "imageSrc": "https://food-images.files.bbci.co.uk/food/recipes/seafoodrisotto_5522_16x9.jpg",
-      "ingredients": [],
+      "ingredients": ["1 lb Prawns", "36 Small Mussels", "50g Squid", "50g Butter", "2 Shallots", "1 Garlic Clove", "350g Risotto Rice", "25g Parmesan", "1 tbsp Olive Oil"],
       "servingSize": "2",
-      "instructions": []
+      "instructions": ["Peel prawns, cook seafood in large saucepan", "Melt butter in large pan, add shallots, salt and risotto rice", "Cook risotto for 20 minutes on medium-low heat", "Add seafood to risotto to allow absorption of flavors", "Enjoy!", "[NOTE: There are a ton of intermediate steps to this recipe that were too long to list]"]
     }
   ]
 
