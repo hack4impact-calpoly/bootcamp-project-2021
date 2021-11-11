cc64b6162c98fd6ec3112e294570b25c2a560d53
megantrieu <meganatrieu@gmail.com>
2021-11-11 01:07:42 -0800

    json

--- meals.js
+++ meals.js
@@ -1,3 +1,34 @@
+let recipeInfo = {}
+
+function recipeToJSON() {
+  recipeInfo.recipeName = document.getElementById('recipeName').innerHTML;
+  recipeInfo.recipeDescription = document.getElementById('recipeDescription').innerHTML;
+  recipeInfo.recipeImage = document.getElementById('recipeImage').src;
+
+  // get each ingredient
+  var ingredientList = []
+  var liTags = document.getElementById('igList').getElementsByTagName('li');
+  for (var key of Object.keys(liTags)) {
+    ingredientList.push(liTags[key].innerHTML);
+  }
+  recipeInfo.ingredientList = ingredientList;
+  //get each instruction
+  var instructionList = []
+  var liTags = document.getElementById('prep').getElementsByTagName('li');
+  for (var key of Object.keys(liTags)) {
+    instructionList.push(liTags[key].innerHTML);
+  }
+  recipeInfo.steps = instructionList;
+
+  outputJSON(recipeInfo);
+}
+
+function outputJSON(recipeInfo) {
+  document.getElementById("json").textContent = JSON.stringify(recipeInfo, undefined, 2);
+  document.getElementById("pre-formatter").style.padding = "16px 0px";
+  document.getElementById("pre-formatter").style.borderBottom = "4px solid black";
+  document.getElementById("pre-formatter").style.borderTop = "4px solid black";
+}
 
 function addIngredient() {
     var ul = document.getElementById("igList");
