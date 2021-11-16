ccdfc7b7de166976d51683d2d91cc8bc1db2b70d
Noah Yuen <noahyuen19@gmail.com>
2021-11-16 21:54:32 +0100

    finishing requests

--- milestone-3/backend/index.js
+++ milestone-3/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 const mongoose = require('mongoose')
+const Recipe = require('./models/recipeSchema')
 const connection_url = "mongodb+srv://dbUserNoah:dbPassword@cluster0.e7ixt.mongodb.net/RecipesDB?retryWrites=true&w=majority"
 port = 3001
 
@@ -11,6 +13,67 @@ mongoose.connect(connection_url)
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
+
+// routes will go here
+
+// route to get all recipes
+app.get("/api/recipe", async (req, res) => {
+    const recipes = await Recipe.find({})
+    res.send(recipes)
+})
+
+// route to get specific recipe
+app.get("/api/recipe/:recipeName", async (req, res) => {
+    const recipeName = req.params.recipeName;
+    const recipe = await Recipe.findOne({ 
+        recipeName: recipeName 
+    })
+    res.send(recipe)
+})
+
+// route to post a new recipe given a body
+app.post("/api/recipe", async (req, res) => {
+    const { recipeName, description, imageSrc, ingredients, servingSize, instructions } = req.body
+    let recipe = new Recipe({
+        recipeName,
+        description,
+        imageSrc,
+        ingredients,
+        servingSize,
+        instructions
+    })
+    try {
+        recipe = await recipe.save()
+        res.send(`Recipe named ${recipeName} and description: ${description}, added to collection`)
+    } catch (error) {
+        console.log(`error is: ${error.message}`)
+    }
+})
+
+// route to put a new ingredient given a recipe name and new ingredient
+app.put("/api/recipe/:recipeName/ingredient", async (req, res) => {
+    const recipeName = req.params.recipeName
+    const ingredient = req.body.newIngredient
+    try {
+        await Recipe.findOneAndUpdate({ recipeName: recipeName }, { $push: { ingredients : ingredient } } )
+        res.send(`Added: ${ingredient} to ${recipeName}'s ingredient list!`)
+    } catch (error) {
+        console.log(`error is: ${error.message}`)
+    }
+})
+
+// route to put a new instruction given a recipe name and new instruction
+app.put("/api/recipe/:recipeName/instruction", async (req, res) => {
+    const recipeName = req.params.recipeName
+    const instruction = req.body.newInstruction
+    try {
+        await Recipe.findOneAndUpdate({ recipeName: recipeName }, { $push: { instructions: instruction } })
+        res.send(`Added: ${instruction} to ${recipeName}'s instruction list!'`)
+    } catch (error) {
+        console.log(`error is: ${error.message}`)
+    }
+})
+
 
 app.listen(port)
 
