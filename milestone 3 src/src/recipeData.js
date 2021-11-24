2307d6128a81b47e6caf882ca8b33ea9500ae37f
jin <jinwu2003@gmail.com>
2021-11-24 00:02:04 -0800

    Jin Wu - Milestone 3

---
+++ milestone 3 src/src/recipeData.js
@@ -0,0 +1,31 @@
+const recipes = [
+    {
+        "recipeName": "Boiled Eggs",
+        "recipeDescription": "Boiled eggs are among one of the best foods to make and eat. They are super easy and super delicious",
+        "recipeInfo": "Nothing beats a good ol boiled egg! They are very portable and convient to eat on the go! It is an absolute cooking essential",
+        "recipeImage": "https://www.mommyhatescooking.com/wp-content/uploads/2020/01/ninja-foodi-hard-boiled-eggs-3-720x720.jpg",
+        "ingredientList": ["Eggs (Exactly 4)", "1 quart Pot", "1 Gallon of water", "Ice (9 cubes)", "Bowl (any size will do)"],
+        "steps": ["Grab pot and fill with water", "Place pot on stove", "Place the 4 eggs in the pot", "Turn on stove at high heat for 8 minutes", "Place Ice and water in Bowl", "Place Eggs in Ice bowl", "Enjoy your eggs!"],
+        "link": "/boiled"
+    },
+    {
+        "recipeName": "Scrambled Eggs",
+        "recipeDescription": "Scrambled Eggs are a staple breakfast dish and can go well with just about anything!",
+        "recipeInfo": "Scrambled Eggs! Scrambled Eggs are the most essential breakfast item. They go great with everything and even a child cant mess up its preperation!",
+        "recipeImage": "https://assets.epicurious.com/photos/57b35f844924889253994109/16:9/w_1280,c_limit/scrambled-eggs.jpg",
+        "ingredientList": ["Eggs (as much as you want)", "A big pan", "Oil (any will do)", "Salt, Pepper and any spices you have in the kitchen", "Plate", "Bowl", "Fork"],
+        "steps": ["Crack all eggs into bowl", "Abuse eggs with fork", "Put some oil on the pan", "Place pan on stove and heat to medium temperature", "Pour beaten eggs into pan", "Cook", "Add seasonings", "Remove eggs from Pan into a plate", "Enjoy!"],
+        "link": "/scrambled"
+    },
+    {
+        "recipeName": "Steamed Egg",
+        "recipeDescription": "Steamed Eggs are good",
+        "recipeInfo": "Steamed Eggs are my favorite egg dish. They are soft and go great with a plain bowl of rice. It can even substitute soup!",
+        "recipeImage": "https://kirbiecravings.com/wp-content/uploads/2018/06/instant-pot-steamed-egg-5.jpg",
+        "ingredientList": ["Eggs (One costco sized pack will do)", "Big, wide, pot", "1 Gallon of water", "Bowl (Must fit in the big pot)", "Metal thing (To keep bowl off the water)", "Salt, White Pepper, Sesame Oil", "Fork", "Plastic Wrap"],
+        "steps": ["Crack eggs into bowl and beat with fork", "Add an ample amount of salt, white pepper and sesame oil to eggs", "Place Water in big pot and put on stove at medium temperatre", "Put metal thing in pot", "Plastic wrap bowl with egg mixture", "Place bowl onto metal thing inside the pot", "Close pot and wait for 20 minutes", "Enjoy!"],
+        "link": "/steamed"
+    }
+];
+
+export default recipes;
