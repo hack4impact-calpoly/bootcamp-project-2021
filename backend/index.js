1dee5d1da64fcd931bb1ee1465e006890a95319d
jin <jinwu2003@gmail.com>
2021-12-01 19:55:40 -0800

    Jin Wu - Milestone 4

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 const mongoose = require('mongoose')
 const connection_url = "mongodb+srv://newUser:newPassword@cluster0.a9qq6.mongodb.net/RecipesDB?retryWrites=true&w=majority"
 const recipeEndpoints = require("./routes/recipe")
@@ -7,11 +8,10 @@ const recipeEndpoints = require("./routes/recipe")
 mongoose.connect(connection_url)
     .then(() => console.log("Connection Success"))
     .catch((error) => console.log("Could not Connect due to ${error}"))
-app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
-})
+});
 
 app.use("/api/recipe", recipeEndpoints)
 
--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -16,4 +16,60 @@ router.get('/:recipeName', async (req, res) => {
     res.send(recipeName)
 })
 
+router.post("/", async (req, res) => {
+    const { recipeName, recipeDescription, recipeInfo, recipeImage, ingredientList, steps, link } = req.body
+    let recipe = new Recipe({
+        recipeName,
+        recipeDescription,
+        recipeInfo,
+        recipeImage,
+        ingredientList,
+        steps,
+        link
+    });
+    try {
+        recipe = await recipe.save();
+        res.send("${recipeName} added to collection");
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+        console.log('error: ${error.message}');
+    }
+});
+
+router.put("/:recipeName/ingredient", async (req, res) => {
+    const recipeName = req.params.recipeName;
+    const ingredient = req.body.newIngredient;
+    const recipe = await Recipe.findOne({ recipeName: recipeName });
+
+    recipe.ingredientList.push(ingredient);
+
+    try {
+        recipe = await recipe.save();
+        res.send("${ingredient} added to ingredient list")
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+        console.log('error: ${error.message}')
+    }
+});
+
+router.put("/:recipeName/step", async (req, res) => {
+    const recipeName = req.params.recipeName;
+    const step = req.body.newStep;
+    const recipe = await Recipe.findOne({ recipeName: recipeName });
+
+    recipe.steps.push(step);
+
+    try {
+        recipe = await recipe.save();
+        res.send("${stept} added to step list")
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+        console.log('error: ${error.message}')
+    }
+});
+
+
 module.exports = router
