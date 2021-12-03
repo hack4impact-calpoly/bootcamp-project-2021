d052b5270eb3d4bf18fd49c7208d94cb26dbb055
vboo123 <89175545+vboo123@users.noreply.github.com>
2021-12-03 15:07:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
-let carInfo = {
-    carName: document.getElementById("Car_Name"),
-    briefDescription: document.getElementById("Brief_Description"),
-    vehicleImage: document.getElementById("Vehicle_Picture"),
-    carSpecs: [
-      document.getElementById("Car_Specs")
-    ],
-    carFacts: [
-      document.getElementById("Car_Facts")
-    ]
-  }
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route,  Link } from 'react-router-dom';
+import Home from './Components/home';
+import Navbar from './Components/navbar';
+import AboutMe from './Components/AboutMe';
+import CarDetails from './Components/carDetails';
 
-let displayJSON = () => {
-  document.body.innerHTML = JSON.stringify(carInfo);
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>                                                                                                                         
+      <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/onboarding/carDetails" element={<CarDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-
-let addCarSpec = () => {
-  let value = document.getElementById("Car_Specs");
-  let random = document.createElement("li");
-  let newValue = document.getElementById("newCarSpec").value;
-  random.appendChild(document.createTextNode(newValue));
-  value.appendChild(random);
-}
-// this car spec function is meant to take in a new value typed in html and display. I am using the demo method for reference...is that ok? It is similar to what I want to do, or do I need
-//to recode a different way?
-
-let addCarFact = () => {
-  let value = document.getElementById("Car_Facts");
-  let random = document.createElement("ul"); //how do I add it as a numbered list?
-  let newValue = document.getElementById("newCarFact").value;
-  random.appendChild(document.createTextNode(newValue));
-  value.appendChild(random);
-}
-// this car spec function is meant to take in a new value typed in html and display. I am using the demo method for reference...is that ok? It is similar to what I want to do, or do I need
-//to recode a different way?
+export default App;
