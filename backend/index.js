f925e1891928c74e8e4d5c881c21a576377d7491
Storm Randolph <storm.randolph@gmail.com>
2021-12-15 20:22:02 -0800

    connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const mongoose = require('mongoose')
+const connection_url = "mongodb+srv://newUser:2021@cluster0.uhkri.mongodb.net/RecipesDB?retryWrites=true&w=majority"
+mongoose.connect(connection_url).then(() => console.log('successfully connected')).catch((error) => console.error(`Could not connect due to ${error}`))
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
