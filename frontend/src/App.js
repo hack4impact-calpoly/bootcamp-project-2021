2e8a91b1bce1fd81bd466ff0f84a8a10aec5382f
Storm Randolph <storm.randolph@gmail.com>
2021-12-18 21:45:58 -0800

    updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import Home from './components/home';
 import AboutMe from './components/aboutme';
 import Nav from './components/navibar';
 import recipes from './recipeData';
 import Recipepage from './components/recipepage';
 function App() {
+  const[recipe,setRecipe] = useState();
+  useEffect(()=> {
+    const loadRecipes = async () => {
+    fetch('http://localhost:3001/api/recipe')
+    .then(res => res.json())
+    .then(jsondata => setRecipe(jsondata))
+    .catch((error) => console.error(`useEffect failed due to ${error}`))
+  }
+  loadRecipes(); },[]);
   return (
       <div className="App">
         <Router>
@@ -13,17 +23,30 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-me" element={<AboutMe />} />
-        {recipes.map(recipe => <Route exact path={recipe.link} element={
+        <Route path="/Pancakes" element={
                 <Recipepage 
-                  name={recipe.recipeName} 
-                  desc={recipe.recipeDescription}
-                  image={recipe.recipeImage}
-                  ingredients={recipe.ingredientList}
-                  steps={recipe.steps}
-                />
-              }
-              />
-            )};
+                  name={recipe[0].recipeName} 
+                  desc={recipe[0].previewDesc}
+                  image={recipe[0].recipeImage}
+                  ingredients={recipe[0].ingredientList}
+                  steps={recipe[0].instructionList}
+                />}/>
+         <Route path="/french" element={
+                <Recipepage 
+                  name={recipe[1].recipeName} 
+                  desc={recipe[1].previewDesc}
+                  image={recipe[1].recipeImage}
+                  ingredients={recipe[1].ingredientList}
+                  steps={recipe[1].instructionList}
+                />}/>
+           <Route path="/baconandegs" element={
+                <Recipepage 
+                  name={recipe[2].recipeName} 
+                  desc={recipe[2].previewDesc}
+                  image={recipe[2].recipeImage}
+                  ingredients={recipe[2].ingredientList}
+                  steps={recipe[2].instructionList}
+                />}/>
         </Routes>
         </Router>
       </div>
